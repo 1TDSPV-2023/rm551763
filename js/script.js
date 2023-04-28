8952d45cfdfb36b866d1d8c2d12f0647246495c2
Duh0127 <rodatupmocgamer22@gmail.com>
2023-04-27 22:54:54 -0300

    JS com IF ternário e DEFER na tag script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
-// alert("Hello World!");
+/*
+    O HOISTING ou elevação, coloca todas as VAR no topo do programa. Isso faz com que se existirem variáveis com o mesmo nome, eles se sobrepoem, independente do lugar no código.
+
+
+    VAR é um valor que pode ser alterado, mas pode ser alterado em qualquer parte do código, por causa do HOISTING.
+    
+    LET ele é um valor variável, mas ele só pode ser alterado no mesmo bloco, sendo diferente do VAR
+    
+    CONST é um valor que não pode ser alterado quando está sendo executado, PRECISA SER INICIALIZADO
+*/
+
+// var nome = "Luizinho";
+// let nome2 = "Vitinho";
+// const nome3 = "Miguelito";
+
+// console.log(nome);
+// console.log(nome2);
+// console.log(nome3);
+
+// if(nome != ""){
+//     let nome = "Nicolly"
+// }
+
+// console.log("Qual nome vai ser impresso aqui!?", nome);
+
+
+
+
+
+
+// let n1 = 10;
+// let n2 = 5;
+
+// // IF TERNÁRIO - descobrir se o número é par ou impar
+// // ele verifica 0 e 1, onde o 0 é negativo, condição falsa, aplicando o else (impar)
+// let resultado = !(n1 % n2) ? "PAR" : "IMPAR"
+
+// console.log(resultado);
+
+
+
+
+
+// Recuperar um elemento do HTML com a função getElementById(parâmetro);
+
+const mn = document.getElementById("menu");
+
+console.log(mn.textContent);
+
+
