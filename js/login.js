2b4157ab6606280db27d12b54a0f1c6b628cc84d
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-18 22:45:23 -0300

    Verificação TOKEN com LocalStorage

--- js/login.js
+++ js/login.js
@@ -47,12 +47,14 @@ let listaDeUsuarios = [
     {
         nomeCompleto : "Paulo da Selva",
         nomeUsuario : "paulo",
-        senhaUsuario : "123456"
+        senhaUsuario : "123456"    
     },
 ];
 
-localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
+
+
+localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 
 
@@ -79,27 +81,60 @@ addEventListener('click', (evento)=>{
         }
     }
 
+
+
+
+
+    //USUARIO QUE REPRESENTA OS DADOS QUE CHEGAM DO FORMULÁRIO
+    const usuarioLogado = {
+        nomeUsuarioLogado : userInput.value,
+        senhaUsuarioLogado: passwordInput.value
+    };
+
+    //USUARIO QUE VAI REPRESENTAR OS DADOS VALIDADOS
+    let usuarioValidado = {};
+
     let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("listaUser"));
 
 
     if(evento.target.id == "btnSubmit"){
         try{
             listaDeUsuariosRecuperada.forEach((usuario)=>{
-                if(userInput.value == usuario.nomeUsuario && passwordInput.value == usuario.senhaUsuario){
+                if(usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && passwordInput.value == usuario.senhaUsuario){
+                    
+                    usuarioValidado = usuario;
+
                     throw "Usuário VALIDO!!!";
                 }
             });
             throw "Usuário ou Senha Invalido!!!"
         }
         catch (err){
+            
+            const msgStatus = document.querySelector("#info");
+
             if(err == "Usuário VALIDO!!!"){
-                console.log("Usuário VALIDADO!");
-                window.location.href = "../sucesso.html";
+                msgStatus.setAttribute("style", "color:#00ff00;");
+                msgStatus.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} realizou o login com SUCESSO!</strong></span>`;
+
+                //Adicionar o objeto USUÁRIO-VALIDADO no LOCAL-STORAGE
+                localStorage.setItem("user-validado", JSON.stringify(usuarioValidado));
+
+
+                //CRIANDO A AUTENTICAÇÃO
+                let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
+                alert(token);
+
+                localStorage.setItem("user-token", token);
+
+                setTimeout(()=>{
+                    window.location.href = "../sucesso.html";
+                },3000);
             }
             else{
-                console.log("Usuário ou Senha INCORRETA!");
+                msgStatus.setAttribute("style", "color:#ff0000;");
+                msgStatus.innerHTML = `<span><strong>Login ou Usuario INCORRETOS!</strong></span>`;
             }
         }
     }
-
 });
