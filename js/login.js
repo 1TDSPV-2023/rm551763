4bd3a5a5397cd1fa76fe0da4146c2408a6e5d493
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-23 22:09:35 -0300

    Exercicio JS logo Avatar de Login

--- js/login.js
+++ js/login.js
@@ -28,36 +28,42 @@
 // console.log(listaDeUsuarios);
 
 
+//ADICIONE UM ATRIBUTOS A CADA OBJETO, SENDO ELES:
+//  - avatarUsuario
+//
+// ESTE ATRIBUTO DEVE CONTER A URL DA IMAGEM DE AVATAR DO USUARIO
+
+
+
 let listaDeUsuarios = [
     {
         nomeCompleto : "José das Couves",
         nomeUsuario : "jose",
-        senhaUsuario : "123456"
+        senhaUsuario : "123456",
+        avatarUsuario : "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
     },
     {
         nomeCompleto : "João Paulino",
         nomeUsuario : "joao",
-        senhaUsuario : "123456"
+        senhaUsuario : "123456",
+        avatarUsuario : "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
     },
     {
         nomeCompleto : "Maria Tomate",
         nomeUsuario : "maria",
-        senhaUsuario : "123456"
+        senhaUsuario : "123456",
+        avatarUsuario: "https://static.vecteezy.com/ti/vetor-gratis/p3/2002403-homem-com-barba-avatar-personagem-icone-isolado-gratis-vetor.jpg"
     },
     {
         nomeCompleto : "Paulo da Selva",
         nomeUsuario : "paulo",
-        senhaUsuario : "123456"    
+        senhaUsuario : "123456",
+        avatarUsuario : "https://static.vecteezy.com/system/resources/previews/011/675/374/original/man-avatar-image-for-profile-png.png"
     },
 ];
 
 
-
-
 localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
-
-
-
 
 
 addEventListener('click', (evento)=>{
@@ -115,6 +121,7 @@ addEventListener('click', (evento)=>{
 
             if(err == "Usuário VALIDO!!!"){
                 msgStatus.setAttribute("style", "color:#00ff00;");
+                msgStatus.innerHTML = `<span><strong> ${usuarioValidado.avatarUsuario} </strong></span>`;
                 msgStatus.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} realizou o login com SUCESSO!</strong></span>`;
 
                 //Adicionar o objeto USUÁRIO-VALIDADO no LOCAL-STORAGE
--- js/sucesso.js
+++ js/sucesso.js
@@ -1,14 +1,20 @@
 
 
+//ATRAVÉS DO OBJETO 
+
 
 if(localStorage.getItem("user-token") != null){
     const bemVindoEl = document.querySelector("#bemvindo-user");
     const botaoLogout = document.querySelector("#btnSair");
+    const imgUsuario = document.querySelector("#imgUsuario");
+    console.log(imgUsuario);
+
 
     //RECUPERANDO O OBJETO USER-VALIDADO DO LOCAL-STORAGE
     const usuarioValidado = JSON.parse(localStorage.getItem("user-validado"));
 
     bemVindoEl.innerHTML = usuarioValidado.nomeCompleto;
+    imgUsuario.setAttribute("src", usuarioValidado.avatarUsuario);
 
     //REMOVENDO OS ITENS DO LOCAL-STORAGE
     botaoLogout.addEventListener("click", ()=>{
